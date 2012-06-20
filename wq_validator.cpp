88494c2b5e99e4fe9b14a9106c6e5ec3de1e71d1
Victor Alberto Romero <vialrogo@gmail.com>
2012-06-19 20:37:03 -0500

    Peliando con los graficos

--- wq_validator.cpp
+++ wq_validator.cpp
@@ -24,10 +24,28 @@ QVector<QPointF>* WQ_Validator::obtenerVectorDatos(int numDatos, int inicio, int
 
     QVector<QPointF>* vectorSalida = new QVector<QPointF>();
     int saltos = ceil( ((double)(fin-inicio)) / limiteGrafica);
+//    int saltos = 1;
 
-    for (int i = inicio; i < fin; ++i) {
-        vectorSalida->push_back(QPointF(i,vectorDatos.at(numDatos)[i]));
+    int contador=0;
+    int acumulador=0;
+
+    for (int i = inicio; i < fin; ++i)
+    {
+        contador++;
+        acumulador+=vectorDatos.at(numDatos)[i];
+
+        if(contador==saltos)
+        {
+            vectorSalida->push_back(QPointF(i-(saltos/2),acumulador));
+            contador=0;
+            acumulador=0;
+        }
     }
+
+//    for (int i = inicio; i < fin; i+=saltos)
+//    {
+//        vectorSalida->push_back(QPointF(i,vectorDatos.at(numDatos)[i]));
+//    }
 
     return vectorSalida;
 }
